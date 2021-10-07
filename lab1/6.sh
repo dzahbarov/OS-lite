6d710697294eaa0383177b49f632ffaed0eccd5a
Vladimir Dzahbarov <30601518+dzahbarov@users.noreply.github.com>
2021-10-07 17:06:33 +0300

    Update 6.sh

--- lab1/6.sh
+++ lab1/6.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-awk '$3 == "(WW)" || $3 == "(II)"' /var/log/anaconda/X.log | sed -e 's/(II)/Information:/g' -e 's/(WW)/Warning:/g' | sort -k3  -r > full.log
+awk '$3 == "(WW)"' /var/log/anaconda/X.log | sed -e 's/(WW)/Warning:/g' > full.log
+awk '$3 == "(II)"' /var/log/anaconda/X.log | sed -e 's/(II)/Information:/g' >> full.log
 cat full.log
